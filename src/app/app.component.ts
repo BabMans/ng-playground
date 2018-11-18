b2254551fa064f3703ecb7637697d2fee963108f
BabMans <BabMans@example.com>
2018-11-18 23:50:21 +0100

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'ng-playground';
+  // this class contains the logic of the application (controller)
+}
